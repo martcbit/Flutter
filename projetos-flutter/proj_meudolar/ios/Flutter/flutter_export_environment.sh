e5f48645c2b0e89895939ed93d7e67cb7ea618e9
martcbit <martins.allysson@hotmail.com>
2021-05-09 02:44:12 -0300

    1

---
+++ projetos-flutter/proj_meudolar/ios/Flutter/flutter_export_environment.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+# This is a generated file; do not edit or check into version control.
+export "FLUTTER_ROOT=/Users/allyssonmartins/Desktop/flutterSDK/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/allyssonmartins/Documents/Projetos/projetos_Flutter/curso_flutter_alura/proj_meudolar"
+export "FLUTTER_TARGET=/Users/allyssonmartins/Documents/Projetos/projetos_Flutter/curso_flutter_alura/proj_meudolar/lib/main.dart"
+export "FLUTTER_BUILD_DIR=build"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "FLUTTER_BUILD_NAME=1.0.0"
+export "FLUTTER_BUILD_NUMBER=1"
+export "DART_DEFINES=flutter.inspector.structuredErrors%3Dtrue"
+export "DART_OBFUSCATION=false"
+export "TRACK_WIDGET_CREATION=true"
+export "TREE_SHAKE_ICONS=false"
+export "PACKAGE_CONFIG=/Users/allyssonmartins/Documents/Projetos/projetos_Flutter/curso_flutter_alura/proj_meudolar/.dart_tool/package_config.json"
